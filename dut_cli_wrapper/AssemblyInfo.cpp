e59b5a89f9175b487e03570152b6cc7b48b029bc
Release Engineer <ReleaseEngineer@maxlinear.com>
2025-09-25 11:28:24 +0530

    URX PRPLOS Release UPDK_9.1.100

---
+++ dut_cli_wrapper/AssemblyInfo.cpp
@@ -0,0 +1,18 @@
+using namespace System;
+using namespace System::Reflection;
+using namespace System::Runtime::CompilerServices;
+using namespace System::Runtime::InteropServices;
+using namespace System::Security::Permissions;
+
+[assembly:AssemblyTitleAttribute(L"dut_cli_wrapper (WLANRTSYS-53879-Implement-WAVE700-Beamforming)")];
+[assembly:AssemblyDescriptionAttribute(L"")];
+[assembly:AssemblyConfigurationAttribute(L"")];
+[assembly:AssemblyCompanyAttribute(L"MaxLinear")];
+[assembly:AssemblyProductAttribute(L"dut_cli_wrapper")];
+[assembly:AssemblyCopyrightAttribute(L"Copyright (c) 2022 MaxLinear. All rights reserved.")];
+[assembly:AssemblyTrademarkAttribute(L"")];
+[assembly:AssemblyCultureAttribute(L"")];
+
+[assembly:AssemblyVersionAttribute("2.26.0")];
+
+[assembly:ComVisible(false)];
